c7c80303e5d12e7baeffbcc6e8ad8758e82d013f
kilona57 <kondrashka04@gmail.com>
2025-01-22 22:01:01 +0300

    Creating models, the main page of the application, the user registration and authorization page., the profile page of the user with his data and goals

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.MainPageView.as_view(), name ='main_page'),
-]
+    path('registration/', views.RegistrationView.as_view(), name ='registration'),
+    path('login/', views.login_user, name ='login'),
+    path('logout/', views.user_logout, name ='logout'),
+    path('main_page_for_logout_user/', views.main_page_for_logout_user, name ='main_page_for_logout_user'),
+    path('password-change/', views.CustomPasswordChangeView.as_view(), name='password_change'),
+    path('userinfo/', views.AddBodyParams.as_view(), name='userinfo'),
+
+    path('profile/', views.profile_view, name='profile'),
+    path('add_body_params/', views.add_body_params, name='add_body_params')
+   ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- healthfit/wsgi.py
+++ healthfit/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'healthfits.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'healthfit.settings')
 
 application = get_wsgi_application()
